5cc27ad98f56286da0046c206bcabf293dde9c3c
cutiebh <cutiebh@gmail.com>
2025-11-18 10:23:15 +0000

    Corrected linting issues

--- tests/test_integration_app.py
+++ tests/test_integration_app.py
@@ -1,11 +1,10 @@
-import json
 from app import app
+
 
 def test_integration_home():
     client = app.test_client()
-
-    # Full request → response cycle
     response = client.get("/")
+
     assert response.status_code == 200
 
     data = response.get_json()
--- tests/test_unit_app.py
+++ tests/test_unit_app.py
@@ -1,9 +1,11 @@
 from app import app
 
+
 def test_home_status_code():
     test_client = app.test_client()
     response = test_client.get("/")
     assert response.status_code == 200
+
 
 def test_home_message():
     test_client = app.test_client()
