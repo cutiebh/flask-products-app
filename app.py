09090266cac130edb1ee0c52ffe5b2b7c0f50593
cutiebh <cutiebh@gmail.com>
2025-11-18 18:57:46 +0000

    Fixed lint issues in app.py

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
-
-""" @app.route("/")
-def home():
-    return jsonify({"message": "Cutie's Flask App is running!"})
- """
 
 @app.route("/")
 def home():
@@ -16,13 +11,22 @@ def home():
             <title>Flask Products App</title>
         </head>
         <body style="font-family: Arial; margin: 40px;">
-            <h1>Welcome to Flask Products App</h1>
+            <h1>Welcome to Cutie's Products App</h1>
             <p>This server is running on EC2 and deployed automatically using Terraform & GitHub Actions.</p>
             <hr>
             <p>Status: <strong>Online</strong></p>
         </body>
     </html>
     """
+
+
+@app.route("/api/info")
+def info():
+    return {
+        "service": "Flask Products App",
+        "status": "running"
+    }
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
